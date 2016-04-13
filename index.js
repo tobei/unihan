501c9ae88dbb88c545746e4e5827fa03416ef107
remy <pub@drijkoningen.net>
2016-04-13 23:19:35 +0200

    extracting values

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-exports.printMsg = function() {
-    console.log("This is a message from the demo package");
-}
+'use strict';
+
+const unzip = require('unzip');
+const request = require('request');
+const csv = require('fast-csv');
+const assert = require('assert');
+
+const handlers = new Map();
+const database = Object.create(null);
+
+request('http://www.unicode.org/Public/UCD/latest/ucd/Unihan.zip')
+    .pipe(unzip.Parse())
+    .on('entry', entry => {
+        entry.pipe(csv({delimiter: '\t', ignoreEmpty: true, comment: '#'}))
+        .on('data', ([character, property, value, ...errors]) => {
+            assert(errors.length == 0);
+            console.log(value);
+        })
+    });
+
