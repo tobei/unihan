4c6de8109e4abcfb568f34f121d1bb7f9475191e
remy drijkoningen <remy@drijkoningen.net>
2016-05-23 15:37:51 +0200

    temporarily add unihan.zip to project for local testing

--- index.js
+++ index.js
@@ -21,29 +21,41 @@ function registerField(name) {
 
 const database = new Map();
 
-fs.createReadStream('unihan.zip')
-    .pipe(unzip.Parse()).on('entry', file => {
-        file.pipe(csv({delimiter: '\t', ignoreEmpty: true, comment: '#'})).on('data', ([character, property, value, ...errors]) => {
-            const action = handlers.get(property);
-            if (action != null) {
-                const informations = database.get(character) || Object.create(null);
-                informations[property] = action.values(value);
-                database.set(character, informations);
-            }
-        })
-    }
-);
+const unzipStream = unzip.Parse();
 
-setTimeout(merge, 10000);
+const permcsvStream = csv({delimiter: '\t', ignoreEmpty: true, comment: '#'})
+    .on('data', process)
+    .on('end', merge);
+
+permcsvStream.end = () => {}
+
+fs.createReadStream('unihan.zip').pipe(unzipStream).on('entry', entry => {
+    console.log(entry.path);
+    //const csvStream = csv({delimiter: '\t', ignoreEmpty: true, comment: '#'}).on('data', process);
+    //entry.unpipe(permcsvStream);
+    //permcsvStream.writable = true;
+    //permcsvStream._writableState.ending = false;
+    //permcsvStream._writableState.ended = false;
+    //permcsvStream._writableState.finished = false;
+    entry.pipe(permcsvStream);
+});
+
 
 const result = Object.create(null);
 
-function merge() {
-    for (let [code, informations] of database) {
+function process([character, property, value, ...errors]) {
+    const action = handlers.get(property);
+    if (action != null) {
+        const informations = database.get(character) || Object.create(null);
+        informations[property] = action.values(value);
+        database.set(character, informations);
+    }
+}
 
-        //console.log(parseInt(code.substring(2), 16));
+function merge() {
+    console.log('now MERFGING');
+    for (let [code, informations] of database) {
         const character = String.fromCodePoint(parseInt(code.substring(2), 16));
-        //console.log(character);
         result[character] = {
             pinyin: readings(informations),
             definition: informations.kDefinition
